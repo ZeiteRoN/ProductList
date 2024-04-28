43ca815b9be9b0ebce386c1cd7c8a0dec0a7916e
ZeiteRoN <zeiter1007@gmail.com>
2024-04-28 17:26:02 +0300

    Init and Home Page

---
+++ application/views/Partials/nav.php
@@ -0,0 +1,18 @@
+<?php ?>
+
+<div class="bg-blue-300 h-20 flex justify-between items-center">
+	<div class="text-5xl">
+		<div>Product List</div>
+	</div>
+	<div class="mr-2 flex gap-3 items-center text-2xl">
+		<div>
+			<a href="">Add new</a>
+		</div>
+		<div>
+			<a href="">Contact</a>
+		</div>
+		<div>
+			<a href="">Log in</a>
+		</div>
+	</div>
+</div>
